d8a02c2d144b3f48dd3b3574b906e706f5c8cab5
Juanes <juanespachon1203@gmail.com>
2024-04-18 22:04:08 -0500

    Add Mobile Dark mode and pulish some details

---
+++ js/dark-mode.js
@@ -0,0 +1,30 @@
+const darkBotton = document.querySelector(".right-container__dark-mode");
+const lightBotton = document.querySelector(".right-container__light-mode");
+const body = document.querySelector("body");
+
+document.addEventListener("DOMContentLoaded", () => {
+  // event that will act just when the html loads without waiting the CSS
+  const darkModeEnabled = localStorage.getItem("darkModeEnabled"); // localstorage saves things in "cache"
+  if (darkModeEnabled) {
+    toggleDarkMode();
+  }
+});
+
+function toggleDarkMode() {
+  body.classList.toggle("dark-mode");
+  toggleAction();
+}
+
+function toggleAction() {
+  const isDarkMode = body.classList.contains("dark-mode");
+
+  if (isDarkMode) {
+    darkBotton.style.display = "none";
+    lightBotton.style.display = "inline";
+    localStorage.setItem("darkModeEnabled", true);
+  } else {
+    lightBotton.style.display = "none";
+    darkBotton.style.display = "inline";
+    localStorage.removeItem("darkModeEnabled");
+  }
+}
