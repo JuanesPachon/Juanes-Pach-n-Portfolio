00382e412c5ba7923d32373a3bdd14c5c99cd7ba
Juanes <juanespachon1203@gmail.com>
2024-04-19 22:36:55 -0500

    Add Desktop Page

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,5 +1,9 @@
 const darkBotton = document.querySelector(".right-container__dark-mode");
 const lightBotton = document.querySelector(".right-container__light-mode");
+
+const darkBotton2 = document.querySelector(".right-container__dark-mode2");
+const lightBotton2 = document.querySelector(".right-container__light-mode2");
+
 const body = document.querySelector("body");
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -21,10 +25,16 @@ function toggleAction() {
   if (isDarkMode) {
     darkBotton.style.display = "none";
     lightBotton.style.display = "inline";
+    darkBotton2.style.display = "none";
+    lightBotton2.style.display = "inline";
     localStorage.setItem("darkModeEnabled", true);
   } else {
     lightBotton.style.display = "none";
     darkBotton.style.display = "inline";
+    lightBotton2.style.display = "none";
+    darkBotton2.style.display = "inline";
     localStorage.removeItem("darkModeEnabled");
   }
 }
+
+
--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const textElement = document.querySelector('.animated-text');
+const textElement2 = document.querySelector('.animated-text-2');
 const textArray = ['Web Developer'];
 let currentIndex = 0;
 
@@ -30,3 +31,32 @@ function startAnimation() {
 }
 
 startAnimation();
+
+function typeWriter2(text, index, callback) {
+  if (index < text.length) {
+    textElement2.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(() => typeWriter2(text, index, callback), 50);
+  } else {
+    setTimeout(callback, 2200);
+  }
+}
+
+function deleteText2() {
+  const currentText2 = textElement2.innerHTML;
+  if (currentText2.length > 0) {
+    textElement2.innerHTML = currentText2.slice(0, -1);
+    setTimeout(deleteText2, 50);
+  } else {
+    currentIndex = (currentIndex + 1) % textArray.length;
+    setTimeout(startAnimation2, 500);
+  }
+}
+
+function startAnimation2() {
+  const text = textArray[currentIndex];
+  textElement2.innerHTML = '';
+  typeWriter2(text, 0, deleteText2);
+}
+
+startAnimation2();
